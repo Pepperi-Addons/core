f7f8edc7dec68a50a209fccedc66c386cdb9bdd1
Saar Guttman <saar.g@pepperi.com>
2022-07-19 16:46:39 +0300

    Refactor

--- server-side/api.ts
+++ server-side/api.ts
@@ -1,26 +1,6 @@
 import { Client, Request } from '@pepperi-addons/debug-server';
-import { CoreService } from './core.service';
-import { Helper } from './helper';
-import PapiService from './papi.service';
+import { resources } from './data_source_api';
 
-export async function get_by_key(client: Client, request: Request) 
-{
-	console.log(`Query received: ${JSON.stringify(request.query)}`);
-
-	switch (request.method) 
-	{
-	case "GET": {
-		const papiClient = Helper.getPapiClient(client);
-		const papiService = new PapiService(papiClient);
-		const core = new CoreService(request.query?.resource_name, request, papiService);
-
-		return await core.getByKey();
-	}
-	default: {
-		throw new Error(`Unsupported method: ${request.method}`);
-	}
-	}
-}
 
 export async function get_items_by_key(client: Client, request: Request) 
 {
@@ -46,7 +26,7 @@ async function getResourceItemByKey(client: Client, request: Request, resourceNa
 	case "GET": {
         const requestCopy = {...request};
         requestCopy.query.resource_name = resourceName;
-		return get_by_key(client, request);
+		return resources(client, request);
 	}
 	default: {
 		throw new Error(`Unsupported method: ${request.method}`);
--- server-side/data_source_api.ts
+++ server-side/data_source_api.ts
@@ -1,4 +1,5 @@
 import { Client, Request } from '@pepperi-addons/debug-server';
+import { CoreService } from './core.service';
 import { CoreSchemaService } from './coreSchema.service';
 import { Helper } from './helper';
 import PapiService from './papi.service';
@@ -34,6 +35,27 @@ export async function purge(client: Client, request: Request)
 		const core = new CoreSchemaService(request.body.Name, request, papiService);
 
 		return await core.purgeSchema();
+	}
+	default: {
+		throw new Error(`Unsupported method: ${request.method}`);
+	}
+	}
+}
+
+export async function resources(client: Client, request: Request) 
+{
+	console.log(`Query received: ${JSON.stringify(request.query)}`);
+
+	switch (request.method) 
+	{
+	case "GET": {
+		const papiClient = Helper.getPapiClient(client);
+		const papiService = new PapiService(papiClient);
+		const core = new CoreService(request.query?.resource_name, request, papiService);
+
+		if(request.query.key){
+			return await core.getByKey();
+		}
 	}
 	default: {
 		throw new Error(`Unsupported method: ${request.method}`);
